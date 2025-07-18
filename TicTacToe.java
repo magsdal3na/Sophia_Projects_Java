954b87028fd875456d33766e1414bbb385d1b918
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2025-07-18 15:44:26 -0500

    Update TicTacToe.java
    added for loop to control player input and fixed outputs when wrong selections are made.

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 public class TicTacToe
 {
 	public static void main(String[] args) {
-	int col;
-	int row;
+
 	Scanner input = new Scanner(System.in);
 	String[][] board = {{" - ", " - ", " - "},
                         {" - ", " - ", " - "},
@@ -15,37 +14,50 @@ public class TicTacToe
     System.out.println("\t" + Arrays.toString(board [1]));
     System.out.println("\t" + Arrays.toString(board [2]) + "\n");
     
-    // Display the prompt text 
-    System.out.print("X - Select row (1-3) & select column  (1-3) ");
-    // Space separates row & column
-    System.out.print("separated by a space: ");
-    row = input.nextInt();
-    col = input.nextInt();
-    // Adding the X to the board
-    board[row - 1][col - 1] = " X ";
-    System.out.println("\nTic-Tac-Toe Board: \n");
-    System.out.println("\t" + Arrays.toString(board[0]));
-    System.out.println("\t" + Arrays.toString(board[1]));
-    System.out.println("\t" + Arrays.toString(board[2]) + "\n");
+    // Defining col and row to help with validation
+    int col = 0;
+    int row = 0;
+    // Defining player char
+    char player;
     
-    // Prompting player using O 
-    System.out.print("O - Select row (1-3) & select column (1-3) ");
-    System.out.print("separated by a space: ");
-    row = input.nextInt();
-    col = input.nextInt();
-    // Mark requested square while subtracting 1 
-    board[row - 1][col - 1] = " O ";
-    System.out.println("\t" + Arrays.toString(board[0]));
-    System.out.println("\t" + Arrays.toString(board[1]));
-    System.out.println("\t" + Arrays.toString(board[2]) + "\n");
-    
-    // Checking if square is already marked
-    if(board[row - 1][col - 1].equals(" - ")) {
-        board[row - 1][col - 1] = " O ";
-    }
-    else {
-        System.out.println("Sorry, that spot is taken.");
-        System.out.println("Your turn will be skipped.");
+    // For loop that runs the 9 rounds  
+    for(int turn = 0; turn < 9; turn++) {
+        // Checking for who's turn it is
+        if(turn % 2 == 0) {
+            player = 'X';
+        }
+        else {
+            player = 'O';
+        }
+        // Check if column & row are legitimate integers
+        while(col < 1 || col > 3 || row < 1 || row > 3){
+            System.out.print(player + " - Select row (1 - 3) & select column (1 - 3) ");
+            System.out.print("separated by a space: ");
+            row = input.nextInt();
+            col = input.nextInt();
+        }
+
+        // Checking for available space
+        if(board[row - 1][col - 1].equals(" - ")) {
+            board[row - 1][col - 1] = " " + player + " ";
+        }
+        else {
+            System.out.println("Sorry, that spot is taken.");
+            System.out.println("Try again.");
+            // Roll loop back
+            turn--;
+        }
+        // Resetting col and row to reset validation
+        col = 0;
+        row = 0;
+        System.out.println("\t" + Arrays.toString(board[0]));
+        System.out.println("\t" + Arrays.toString(board[1]));
+        System.out.println("\t" + Arrays.toString(board[2]) + "\n");
+        
+        // If 9th turn, end after X's turn
+        if(turn == 9) {
+            break;
+        }
     }
 	}
 }
